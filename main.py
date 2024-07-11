63a27b9d8fb1eee0f2c33d27cd893b26ac9c3166
ssymhoven <ssymhoven@donner-reuschel.lu>
2024-07-11 16:58:03 +0200

    add Dax

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from utility import get_universe_data, plot_histogram, get_first_dezil, style_un
 
 spx = get_universe_data(universe="S&P 500")
 sxxp = get_universe_data(universe="STOXX Europe 600")
-
+dax = get_universe_data(universe="DAX Index")
 
 if __name__ == '__main__':
     plot_histogram(spx, 'S&P 500')
     plot_histogram(sxxp, 'STOXX Europe 600')
+    plot_histogram(dax, 'DAX')
 
     first_dezil_spx_df = get_first_dezil(spx, 'momentum_score')
     style_universe_with_bars(first_dezil_spx_df, "S&P 500")
 
     first_dezil_sxxp_df = get_first_dezil(sxxp, 'momentum_score')
     style_universe_with_bars(first_dezil_sxxp_df, "STOXX Europe 600")
+
+    first_dezil_dax_df = get_first_dezil(dax, 'momentum_score')
+    style_universe_with_bars(first_dezil_dax_df, "DAX Index")
+
